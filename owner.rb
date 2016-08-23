5ba82280c9efe0c0e63952da9ec891d6b442c146
Shari <smeggs1@gmail.com>
2016-08-23 11:01:28 -0700

    This has completed the first option listed in wave 1. Would like to add the second with a method that would link an account

---
+++ owner.rb
@@ -0,0 +1,22 @@
+require_relative 'account'
+
+module Bank
+#Still need to add option 2! Possibly create a method that will automatically add an author
+  class Owner
+    attr_accessor :name, :dob, :address, :city, :state
+
+    def initialize
+      @name = name
+      @dob = birth_date
+      @address = address
+      @city = city
+      @state = state
+    end
+
+    def to_s
+      puts "name: #{name} \n Date of birth: #{dob} \n Street Address: #{address} \n City: #{city}
+      State: #{state}"
+    end
+
+  end
+end
