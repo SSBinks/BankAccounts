c66e397dfe78cc4278fd9a1ab140e14cdc1608bf
Shari Meggs <smeggs1@gmail.com>
2016-08-26 16:11:00 -0700

    Built out the array of owners using the CSV

--- owner.rb
+++ owner.rb
@@ -1,22 +1,48 @@
 require_relative 'account'
+require 'csv'
 
 module Bank
 #Still need to add option 2! Possibly create a method that will automatically add an author
   class Owner
-    attr_accessor :name, :dob, :address, :city, :state
+    attr_accessor :address, :city, :state
+    attr_reader :ssn, :last_name, :first_name
 
-    def initialize
-      @name = name
-      @user_id = id
-      @dob = birth_date
+    def initialize(ssn, last_name, name, address, city, state)
+      @ssn = ssn
+      @last_name = last_name
+      @first_name = name
       @address = address
       @city = city
       @state = state
     end
 
+    def self.all
+      account_holder = {}
+      accounts = []
+      CSV.open('/Users/ShariMeggs/Documents/ada/Wave-Projects/BankAccounts/BankAccounts/support/owners.csv','r').each do |person|
+        account_holder[person[0]] = person[1..5]
+        ssn = person[0].to_i
+        last_name = person[1]
+        first_name = person[2]
+        address = person[3]
+        city = person[4]
+        state = person[5]
+        accounts << self.new(ssn, last_name, first_name, address, city, state)
+      end
+      return accounts
+    end
+
+    def self.find(user)
+      user_search = self.all
+      user_search.each do |peeps|
+        if peeps.ssn == user
+          return peeps
+        end
+      end
+    end
+
     def to_s
-      puts "name: #{name} \n Date of birth: #{dob} \n Street Address: #{address} \n City: #{city}
-      State: #{state}"
+      return "Last Name: ##{@last_name} \n First Name: #{@first_name} \n Street Address: #{@address} \n City: #{@city} State: #{@state}"
     end
 
   end
